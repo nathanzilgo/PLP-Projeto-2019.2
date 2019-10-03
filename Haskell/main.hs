dfc4c4d1b191778be031f98df2aaa0324242e0ae
nathan <nathanfpedroza@gmail.com>
2019-10-02 22:14:10 -0300

    listas de fronteiras adicionada

---
+++ Haskell/main.hs
@@ -0,0 +1,15 @@
+view_selector :: Int -> String
+    -- TODO
+
+main = do
+    -- Fronteiras de cada estado:
+    front_pb = ["RN", "PE", "CE"]
+    front_pe = ["AL", "BA", "PB", "PI", "CE"]
+    front_ba = ["SE", "AL", "PE", "PI"]
+    front_pi = ["MA", "BA", "CE", "PE"]
+    front_ma = ["PI"]
+    front_ce = ["RN", "PI", "PE", "PA"]
+    front_rn = ["CE", "PA"]
+    front_al = ["PE", "SE", "BA"]
+    front_se = ["BA", "AL"]
+
