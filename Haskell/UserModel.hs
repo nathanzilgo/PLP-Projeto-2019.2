a5e6bad8575115c69449b7f99065bb9616bc99c2
nathan <nathanfpedroza@gmail.com>
2019-10-10 17:31:55 -0300

    Bot e User estão na mesma entidade User. Dependencia de Tuplas instalada. Inicio do desenvolvimento de métodos aleatorios.

--- Haskell/UserModel.hs
+++ Haskell/UserModel.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
--- Definição de um User do jogo que contem os métodos aplicados
+-- Definição de um User do jogo (Humano ou bot) que contem os métodos aplicados
 -- ao User em si.
 module User where
 
 import Data.text (Text)
+import Data.Tuple.Select -- Utilitários para tuplas. Precisa da dependência de tuplas instalada (veja o README)
 
 data User = User{
     name :: String,
@@ -15,8 +16,9 @@ data User = User{
 -- Método para alocar tropas livres no início de um round para um User.
 -- Params: User user, Int quantidade, String estado.
 -- Return: String resultado
-alocaTroops :: User -> Int -> String -> IO()
-
+alocaTroops :: User -> Int -> String -> User
+alocaTroops usr 0 _ = usr
+    | usr num estado = if()
 
 -- Método para realocar tropas de um User de um estado para outro.
 -- Params: User user, Int quantidade, String estado_remove, String estado_add.
@@ -28,3 +30,15 @@ realocaTroops :: User -> Int -> String -> String -> IO()
 -- Params: User user, Int quantidade, String estado.
 -- Return: String resultado.
 removeTroops :: User -> Int -> String -> IO()
+
+randomState :: [(String, Int)] -> String
+
+randomTroops :: Int -> Int
+
+randomPlay :: Int -> Int
+
+-- Verifica se um User tem controle sobre um estado
+-- Params: User user, String estado, [(String, Int)] estadosUsr
+-- Return: Boolean resultado
+possuiEstado :: User -> String -> [(String, Int)] -> Boolean
+possuiEstado user estado h:t = 
