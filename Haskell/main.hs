51469fbb0087232d0392852a83e9cafb7ec0d32b
Antonio Neto <antoniontv18@gmail.com>
2019-10-13 21:18:39 -0300

    Adicionando criacao de bot e usuario no main

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -4,6 +4,8 @@ module Project where
 
 main :: IO
 main = do
+    let user = User "User" 5 0 0 0 0 0 0 0 0 0
+    let bot = User "bot" 5 0 0 0 0 0 0 0 0 0
 
 -- Função para atacar um User a partir de outro
 -- Params: os 2 users
