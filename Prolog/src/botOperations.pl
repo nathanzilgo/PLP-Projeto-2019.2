adf04ea3e5a5b4e24b8909a493778a222fe071b2
Antonio Neto <antoniontv18@gmail.com>
2019-11-13 23:52:41 -0300

    Adicionando estrutura para operações do bot

---
+++ Prolog/src/botOperations.pl
@@ -0,0 +1,22 @@
+:- module(botOperations, [
+        botAttack/2,
+        botAllocateTerritorie/1,
+        botAllocateTerritorie/3
+        ]).
+    
+    :- use_module('player.pl').
+    
+    %Metodo usado para o BOT atacar
+    botAttack(Troops, Territorie) :-
+        write("TODO").
+    
+    %Metodo para alocar um determinado Territorio para o BOT
+    botAllocateTerritorie(Territorie) :-
+        write("TODO").
+    
+    % Metodo para mover tropas de um territorio para outro territorio de um BOT
+    botReallocateTrooops(Quantity, TerritorieToLoose, TerritorieToWin) :-
+        write("TODO").
+    
+    
+    
