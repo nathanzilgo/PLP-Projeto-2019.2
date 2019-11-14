f61ebdd5f1db732de6f5154fdbed7a32d6e2e070
Antonio Neto <antoniontv18@gmail.com>
2019-11-13 23:50:54 -0300

    Adicionando estrutura para operações do player

---
+++ Prolog/src/playerOperations.pl
@@ -0,0 +1,20 @@
+:- module(playerOperations, [
+    attack/2,
+    playerAllocateTerritorie/1
+    ]).
+
+:- use_module('player.pl').
+
+%Metodo usado para o player atacar
+playerAttack(Troops, Territorie) :-
+    write("TODO").
+
+%Metodo para alocar um determinado Territorio para um player
+playerAllocateTerritorie(Territorie) :-
+    write("TODO").
+
+% Metodo para mover tropas de um territorio para outro do player
+playerReallocateTrooops(Quantity, TerritorieToLoose, TerritorieToWin) :-
+    write("TODO").
+
+
