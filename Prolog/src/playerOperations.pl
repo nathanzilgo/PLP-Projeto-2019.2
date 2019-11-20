2001a9b1b389d100b7b76edefda0b91e12a0ab44
nathan <nathanfpedroza@gmail.com>
2019-11-19 23:51:38 -0300

    start Main.pl structure

--- Prolog/src/playerOperations.pl
+++ Prolog/src/playerOperations.pl
@@ -1,21 +1,21 @@
+:- use_module('player.pl').
+
 :- module(playerOperations, [
     playerAttack/2,
-    playerAllocateTerritorie/1,
+    playerAllocateTerritory/1,
     playerReallocateTrooops/3
     ]).
 
-:- use_module('player.pl').
-
 %Metodo usado para o player atacar
-playerAttack(Troops, Territorie) :-
+playerAttack(Troops, Territory) :-
     write("TODO").
 
 %Metodo para alocar um determinado Territorio para um player
-playerAllocateTerritorie(Territorie) :-
+playerAllocateTerritory(Territory) :-
     write("TODO").
 
 % Metodo para mover tropas de um territorio para outro do player
-playerReallocateTrooops(Quantity, TerritorieToLoose, TerritorieToWin) :-
+playerReallocateTrooops(Quantity, TerritoryToLoose, TerritoryToWin) :-
     write("TODO").
 
 
