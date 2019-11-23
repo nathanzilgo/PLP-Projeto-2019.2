f7a2acff32b009f2850b7668350b462c6c69bb9d
Antonio Bertino <antonio.bertino.neto@ccc.ufcg.edu.br>
2019-11-23 15:10:16 -0300

    Adicionando fronteiras ao projeto

---
+++ Prolog/src/fronteiras.pl
@@ -0,0 +1,38 @@
+:- module(frontier, [
+        frontier/1
+        ]).
+
+fronteira("Paraiba", "RioGrandeDoNorte").
+fronteira("Paraiba", "Pernambuco").
+fronteira("Paraiba", "Ceara").
+fronteira("Pernambuco", "Alagoas").
+fronteira("Pernambuco", "Bahia").
+fronteira("Pernambuco", "Paraiba").
+fronteira("Pernambuco", "Piaui").
+fronteira("Pernambuco", "Ceara").
+fronteira("Bahia", "Sergipe").
+fronteira("Bahia", "Alagoas").
+fronteira("Bahia", "Pernambuco").
+fronteira("Bahia", "Piaui").
+fronteira("Piaui", "Maranhao").
+fronteira("Piaui", "Bahia").
+fronteira("Piaui", "Ceara").
+fronteira("Piaui", "Pernambuco").
+fronteira("Maranhao", "Piaui").
+fronteira("Ceara", "RioGrandeDoNorte").
+fronteira("Ceara", "Piaui").
+fronteira("Ceara", "Pernambuco").
+fronteira("Ceara", "Paraiba").
+fronteira("RioGrandeDoNorte", "Ceara").
+fronteira("RioGrandeDoNorte", "Paraiba").
+fronteira("Alagoas", "Pernambuco").
+fronteira("Alagoas", "Sergipe").
+fronteira("Alagoas", "Bahia").
+fronteira("Sergipe", "Bahia").
+fronteira("Sergipe", "Alagoas").
+
+
+
+
+frontier(State1, State2) :-
+    fronteira(State1, State2).
