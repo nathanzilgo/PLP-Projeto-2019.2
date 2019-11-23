6782aedb0661af219a0d81a6f4b75bb69116fc2f
Antonio Bertino <antonio.bertino.neto@ccc.ufcg.edu.br>
2019-11-23 14:26:08 -0300

    Removendo metodo de attack e passando para o gameOperations

--- Prolog/src/playerOperations.pl
+++ Prolog/src/playerOperations.pl
@@ -6,10 +6,6 @@
 
 :- use_module('player.pl').
 
-%Metodo usado para o player atacar
-playerAttack(Troops, Territory) :-
-    write("TODO").
-
 %Metodo para alocar um determinado Territorio para um player
 playerAllocateTerritory(Territory) :-
     updateStateTroops("PLAYER", Territory, 1).
