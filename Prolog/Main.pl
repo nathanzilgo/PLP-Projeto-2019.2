83e549e1f0f4a4a0841fd2a5ac3be659bc0e5911
nathan <nathanfpedroza@gmail.com>
2019-11-29 03:28:45 -0300

    add nextRound(), bot Main and user Main

--- Prolog/Main.pl
+++ Prolog/Main.pl
@@ -1,5 +1,5 @@
-:- initialization main, halt.
-
+:- initialization showTitle, main(0), halt.
+% inicializa o jogo com titulo, main(0) e se sair da main, halt encerra o programa.
 /*
 :- module(main, [
     showTitle,
@@ -30,8 +30,9 @@ showOptions :-
     write('1) Alocar tropas'),nl,
     write('2) Realocar tropas existentes'),nl,
     write('3) Atacar'),nl,
-    write('4) Encerrar jogada'),nl.
-
+    write('4) Encerrar jogada'),nl,
+    write('5) Printar status do jogo'), nl,
+    write('6) Encerrar jogo'), nl.
 
 % Exibir txt na tela
 showOnScreen([H|T]):- 
@@ -47,13 +48,35 @@ allocateTroopsView(Id, Troops, State):-
     write('Insira o nome do estado:'),read(State),
     allocateTroops(Id, Troops, State).
 
+reallocateTroopsView(Quantity, TerritoryToLoose, TerritoryToWin):-
+    write('Insira a quantidade de tropas para realocar'), read(Quantity),
+    write('De onde deseja tirar?'), read(TerritoryToLoose),
+    write('Aonde deseja colocar?'), read(TerritoryToWin),
+    playerReallocateTroops(Quantity, TerritoryToLoose, TerritoryToWin).
+
+playerAttackView(Atacante, Defensor):-
+    write('De que estado deseja atacar?'), read(Atacante),
+    write('Qual estado deseja atacar?'), read(Defensor),
+    playerAttack(Atacante, Defensor).
 
 optChosse(1, Call) :- Call is allocateTroopsView(_,_,_).
-optChosse(2, Call) :- Call is playerAllocateTerritory(Territory).
+optChosse(2, Call) :- Call is reallocateTroopsView(_,_,_).
+optChoose(3, Call) :- Call is playerAttackView(_,_).
+optChoose(4, Call) :- Call is nextRound().
 
-main :- 
+% Define quem irá jogar em seguida.
+nextRound('bot'):- main('player').
+nextRound('player'):- main('bot').
+
+% 0 é o player humano
+main(0) :- 
     showTitle(),
     showOptions,
-    getInput(Inp, Retorno).
+    getInput(Inp, Retorno),
+    main.
 
-
+% main do bot(1)
+main(1) :- 
+    showOptions,
+    getInput(Inp, Retorno),
+    main.
